2d3ccb2a0e6863b58e4e629cf9438e39792c31fc
Carole Gérard <gerard.carole78@gmail.com>
2024-04-13 10:57:33 +0200

    An Important Note about Route Observales

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 
 @Component({
@@ -9,9 +10,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 
 
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
 
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
@@ -26,13 +28,21 @@ export class UserComponent implements OnInit {
 
     // Souscription aux changements des paramètres de l'URL.
     // Ceci est utile pour réagir aux changements des paramètres après le chargement initial du composant.
-    this.activatedRoute.params
+    this.paramsSubscription = this.activatedRoute.params
     .subscribe(
       (params: Params) => {        
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     )
+  }
+
+  ngOnDestroy() {
+    // Cette méthode est appelée avant la destruction du composant.
+    // Il est recommandé de se désabonner de toutes les souscriptions pour éviter les fuites de mémoire.
+
+    // ⬇️ Ce désabonnement n'est pas obligatoire car Angular le fait automatiquement en ce qui concerne les souscriptions aux paramètres de l'URL.
+    this.paramsSubscription.unsubscribe();  
   }
 
 }
