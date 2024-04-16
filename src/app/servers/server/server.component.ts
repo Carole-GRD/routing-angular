6cf027d8e5316e5a59d09469b5010ad874838812
Carole Gérard <gerard.carole78@gmail.com>
2024-04-16 14:58:28 +0200

    Practicing

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
+
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
+
+
 export class ServerComponent implements OnInit {
+
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.activatedRoute.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+
+    this.activatedRoute.params
+      .subscribe(
+        (params: Params) => {  
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
   }
 
 }
