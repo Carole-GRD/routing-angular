a97a6bdad240be540c8a61536519946627aacf58
Carole Gérard <gerard.carole78@gmail.com>
2024-04-16 20:25:31 +0200

    Configuring the Handling of Query Parameters

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -25,10 +26,16 @@ export class EditServerComponent implements OnInit {
     // "queryParams" et "fragment" sont des observables, on s'abonne pour les lire à chaque changement et pas seulement une fois à l'initialisation du composant
     // pas besoin de se désabonner car Angular le fait pour nous comme pour "params"
     // Angular sait que ces observables sont liés à la durée de vie du composant et qu'ils seront détruits en même temps que le composant
-    this.activatedRoute.queryParams.subscribe();
+    this.activatedRoute.queryParams
+      .subscribe(
+        (queryParams: Params) => {
+          this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+        }
+      );
     this.activatedRoute.fragment.subscribe();
     
-    this.server = this.serversService.getServer(1);
+    // this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(+this.activatedRoute.snapshot.params['id']);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
