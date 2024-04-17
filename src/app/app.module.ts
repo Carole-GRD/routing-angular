9f271f6c701ac44364aafae8cacbcb58c71a5e97
Carole Gérard <gerard.carole78@gmail.com>
2024-04-17 09:15:20 +0200

    Redirecting and  Wilcard Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ServersService } from './servers/servers.service';
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { UserComponent } from "./users/user/user.component";
 import { ServersComponent } from "./servers/servers.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -14,5 +15,7 @@ export const routes: Routes = [
     { path: 'servers', component: ServersComponent, children: [
         { path: ':id', component: ServerComponent },
         { path: ':id/edit', component: EditServerComponent }
-    ]}
+    ]},
+    { path: 'not-found', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/not-found' }
 ];
